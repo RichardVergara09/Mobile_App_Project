d4e3d0fdc3863be61011c7a59f441ed3ac2ffc47
RichardVergara09 <richarddeanvergara@gmail.com>
2024-12-03 13:28:36 -0700

    commit

---
+++ components/customize.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const customize = () => {
+  return (
+    <View>
+      <Text>customize</Text>
+    </View>
+  );
+};
+
+export default customize;
+
+const styles = StyleSheet.create({});
