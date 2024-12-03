ef2af63346f7853ec8e3d2de971bfc9ab317fff1
RichardVergara09 <richarddeanvergara@gmail.com>
2024-12-03 13:45:22 -0700

    commit

--- components/gamePage.js
+++ components/gamePage.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const gamePage = () => {
   return (
     <View>
       <Text>gamePage</Text>
     </View>
-  )
-}
+  );
+};
 
-export default gamePage
+export default gamePage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
